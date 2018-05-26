6c7093361fcb077d7d7967a0d87629ccc49c9a75
Isaiah Berg <isaiah.j.berg@gmail.com>
2018-05-25 23:53:57 -0400

    modified dynamic background for effect

--- scripts/script.js
+++ scripts/script.js
@@ -6,5 +6,5 @@ htmlBody.addEventListener('mousemove', dynaBack)
 
 function dynaBack(e){
   console.log(e)
-  document.body.style.background = `linear-gradient(195deg,rgba(0,${e.screenX/2},0,.3),white,rgba(0,${e.screenY/2},0,.5))`
+  document.body.style.background = `linear-gradient(${220-Math.sqrt(4*e.screenY)}deg,rgba(0,${e.screenX+200},0,${(e.screenY/1500)+(e.screenX/3000)}),white,rgba(0,${e.screenX+100},0,${(150/e.screenY)+e.screenX/2000}))`
 }
