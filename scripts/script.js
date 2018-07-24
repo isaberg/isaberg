00a045796f387135df44edc07c12bec9d5e3f0c9
Isaiah Berg <isaiah.j.berg@gmail.com>
2018-07-24 16:18:33 -0400

    modified script for more background transparency and less green

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,7 @@ htmlBody.addEventListener('mousemove', dynaBack)
 
 function dynaBack(e){
   console.log(e)
-  document.body.style.background = `linear-gradient(200deg,rgba(0,${e.screenX+200},0,${(e.screenY/1500)+(e.screenX/3000)}),white,rgba(0,${e.screenX+100},0,${(150/e.screenY)+e.screenX/2000}))`
+  document.body.style.background = `linear-gradient(200deg,rgba(0,${e.screenX+200},0,${(e.screenY/4000)+(e.screenX/8000)}),white,rgba(0,${e.screenX+100},0,${(50/e.screenY)+e.screenX/5000}))`
 }
 
 
