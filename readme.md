f2682259b51ee46c32ddb4e63dd8353b1a639a87
Isaiah Berg <isaiah.j.berg@gmail.com>
2018-07-26 17:13:26 -0400

    portfolio mod complete

--- readme.md
+++ readme.md
@@ -1,12 +1,5 @@
-Style ideas:
-
+Portfolio inspiration: 
 
 https://maninak.github.io/
 
 http://lucasmartin.fr/
-
-To Do:
-
-Color-shifting transparent background
-
-Drop down arrow for bio and for projects/work sub headings.
--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,21 @@
-const htmlBody = document.querySelector('body')
-console.log(htmlBody)
-
-htmlBody.addEventListener('mousemove', dynaBack)
-
-
-function dynaBack(e){
-  console.log(e)
+dynaBack = (e) => {
   document.body.style.background = `linear-gradient(200deg,rgba(0,${e.screenX+200},0,${(e.screenY/4000)+(e.screenX/8000)}),white,rgba(0,${e.screenX+100},0,${(50/e.screenY)+e.screenX/5000}))`
 }
 
+const htmlBody = document.querySelector('body')
+htmlBody.addEventListener('mousemove', dynaBack)
 
-//prior version with moving axis
-//document.body.style.background = `linear-gradient(${220-Math.sqrt(4*e.screenY)}deg,rgba(0,${e.screenX+200},0,${(e.screenY/1500)+(e.screenX/3000)}),white,rgba(0,${e.screenX+100},0,${(150/e.screenY)+e.screenX/2000}))`
+toggleActive = (e) => {
+  console.log('running toggleActive')
+  console.log(e)
+  e.target.classList.toggle('active')
+  e.target.nextElementSibling.nextElementSibling.classList.toggle('active')
+  console.log(e.target.nextElementSibling.nextElementSibling)
+}
+
+var arrows = document.getElementsByClassName('arrow')
+var i
+for (i = 0; i < arrows.length; i++) {
+  console.log('attaching active arrow listeners')
+  arrows[i].addEventListener('click', toggleActive)
+}
